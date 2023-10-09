4c856acbe00038c0cf0d1d19cc9ba49b3319c6ae
osmacpro <osmac@MacBook-Pro-osmacpro.local>
2023-10-09 18:43:19 +0600

    Added homework

--- src/HomeWork2.java
+++ src/HomeWork2.java
@@ -1,5 +1,4 @@
 public class HomeWork2 {
-    public class Main {
         public static void main(String[] args) {
             System.out.println("Hello world!");
             System.out.println("Hello, Java!");
@@ -8,6 +7,5 @@ public class HomeWork2 {
         System.out.println("Hello world!");
         System.out.println("Hello, Java!");
          */
-        }
     }
 }
--- src/HomeWork3.java
+++ src/HomeWork3.java
@@ -1,51 +1,5 @@
-public class Main {
+public class HomeWork3 {
     public static void main(String[] args) {
-        /*System.out.println("Hello world!");
-        //System.out.println("Hello, Java!");
-        System.out.println("Hello, Java!"); */
-        //System.out.println("Hello, Java!");
-        /* System.out.println("Hello world!");
-        System.out.println("Hello, Java!");
-         */
-        /* short a = 20;
-        short b = -20;
-        System.out.println(a + b);
-        System.out.println(a - b);
-        a = 40;
-        System.out.println(a);
-        System.out.println(b);
-        */
-        /* byte a = 10;
-        byte b = -20;
-          short c = 10000;
-          short d = -5000;
-          int e = 100000;
-          int f = -200000;
-          long m = 100000000;
-          long n = -100000000;
-
-          double g = 2.5;
-          float h = 34.8f;
-        System.out.println(g);
-        System.out.println(h);
-
-        char letterA = 'A';
-        System.out.println(letterA);
-
-        boolean flag = true;
-        String str = "AC Milan";
-        System.out.println(str);
-
-        str = "Real Madrid";
-        System.out.println(str);
-
-        int a1 = 5, b1 = 6;
-        System.out.println(a1 + b1);
-
-        int x, y, z;
-        x = y = z = 60;
-        System.out.println(x + y + z);
-        */
         // Задание 1
         String flowersName = "Roses";
         // Задание 2
@@ -69,7 +23,6 @@ public class Main {
         int sum2 = p + q + r + s;
         System.out.println(sum2);
 
-            public static void main(String[] args) {
                 //  Задание 1
                 double myDouble;
                 myDouble = 7;
@@ -106,7 +59,12 @@ public class Main {
                 System.out.println("PI: " + PI);
                 System.out.println("GRAVITY: " + GRAVITY);
                 System.out.println("HOMETOWN: " + HOMETOWN);
-            }
-        }
 
+        // Расширяющее приведение int к double
+        int myInt = 10;
+        double myDouble = myInt;
+        // Сужающее приведение double к int
+        double myDouble = 10.5;
+        int myInt = (int) myDouble;
     }
+}
