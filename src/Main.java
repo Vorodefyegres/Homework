c2f1680133539c813f7c1e7a6afbabe189154729
osmacpro <osmac@MacBook-Pro-osmacpro.local>
2023-10-09 13:58:07 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+        /*System.out.println("Hello world!");
+        //System.out.println("Hello, Java!");
+        System.out.println("Hello, Java!"); */
+        //System.out.println("Hello, Java!");
+        /* System.out.println("Hello world!");
+        System.out.println("Hello, Java!");
+         */
+        /* short a = 20;
+        short b = -20;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        a = 40;
+        System.out.println(a);
+        System.out.println(b);
+        */
+        /* byte a = 10;
+        byte b = -20;
+          short c = 10000;
+          short d = -5000;
+          int e = 100000;
+          int f = -200000;
+          long m = 100000000;
+          long n = -100000000;
+
+          double g = 2.5;
+          float h = 34.8f;
+        System.out.println(g);
+        System.out.println(h);
+*/
+        char letterA = 'A';
+        System.out.println(letterA);
+
+        boolean flag = true;
+        String str = "AC Milan";
+        System.out.println(str);
+
+        str = "Real Madrid";
+        System.out.println(str);
+
+        int a1 = 5, b1 = 6;
+        System.out.println(a1 + b1);
+
+        int x, y, z;
+        x = y = z = 60;
+        System.out.println(x + y + z);
+
+        String name = "Maria";
+        System.out.println("Hello " + name);
+    }
+}
