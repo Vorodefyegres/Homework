5ba06d48baefd38bcf88690b403b4a2d3763f367
osmacpro <osmac@MacBook-Pro-osmacpro.local>
2023-10-11 17:20:01 +0600

    Added homework

--- src/Main.java
+++ src/Main.java
@@ -113,5 +113,12 @@ System.out.println("PI: " + PI);
 System.out.println("GRAVITY: " + GRAVITY);
 System.out.println("HOMETOWN: " + HOMETOWN);
 */
+       int a = 5;
+        System.out.println("a += 10: " + (a += 10));
+        System.out.println("a -= 3: " + (a -= 3));
+        System.out.println("a *= 2: " + (a *= 2));
+        System.out.println("a /= 6: " + (a /= 6));
+        System.out.println("a <<= 4: " + (a <<= 4));
+        System.out.println("a >>= 3: " + (a >>= 3));
     }
 }
